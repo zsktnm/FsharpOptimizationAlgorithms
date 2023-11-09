1135f2a9496fe864d93f42ecde0076491c332ab6
Zsktnm <takraw@yandex.ru>
2023-11-09 11:30:52 +0500

    Bagpack (dynamic)

---
+++ OptimizationProblems.Dynamic.Bagpack.Tests/Tests.fs
@@ -0,0 +1,96 @@
+module OptimizationProblems.Dynamic.Bagpack.Tests
+
+open NUnit.Framework
+
+[<SetUp>]
+let Setup () =
+    ()
+
+[<Test>]
+let ``one item`` () =
+    let weights = [| 2; |]
+    let prices = [| 100; |]
+    let capacity = 4
+    let expected = 100
+    let actual = solve prices weights capacity
+
+    match actual with
+    | Error _ -> Assert.Fail "Result returns error"
+    | Ok value -> Assert.That(value, Is.EqualTo(expected))
+
+[<Test>]
+let ``one item but small backpack`` () =
+    let weights = [| 4; |]
+    let prices = [| 100; |]
+    let capacity = 2
+    let expected = 0
+    let actual = solve prices weights capacity
+
+    match actual with
+    | Error _ -> Assert.Fail "Result returns error"
+    | Ok value -> Assert.That(value, Is.EqualTo(expected))
+
+
+[<Test>]
+let ``two items but can take only one`` () =
+    let weights = [| 4; 3; |]
+    let prices = [| 100; 99 |]
+    let capacity = 5
+    let expected = 100
+    let actual = solve prices weights capacity
+
+    match actual with
+    | Error _ -> Assert.Fail "Result returns error"
+    | Ok value -> Assert.That(value, Is.EqualTo(expected))
+
+
+
+[<Test>]
+let ``four items`` () =
+    let weights = [| 2; 3; 4; 5 |]
+    let prices = [| 1; 2; 5; 6 |]
+    let capacity = 8
+    let expected = 8
+    let actual = solve prices weights capacity
+
+    match actual with
+    | Error _ -> Assert.Fail "Result returns error"
+    | Ok value -> Assert.That(value, Is.EqualTo(expected))
+
+[<Test>]
+let ``seven items`` () =
+    let weights = [| 2; 3; 5; 7; 1; 4; 1 |]
+    let prices = [| 1000; 500; 1500; 700; 600; 1800; 300 |]
+    let capacity = 15
+    let expected = 5400
+    let actual = solve prices weights capacity
+
+    match actual with
+    | Error _ -> Assert.Fail "Result returns error"
+    | Ok value -> Assert.That(value, Is.EqualTo(expected))
+
+
+[<Test>]
+let ``invalid lengths`` () =
+    let weights = [| 2; 3; 4; 5 |]
+    let prices = [| 1; 2; 5; 6; 8 |]
+    let capacity = 8
+    let actual = solve prices weights capacity
+
+    match actual with
+    | Error _ -> Assert.Pass ()
+    | Ok value -> Assert.Fail ()
+
+[<Test>]
+let ``empty arrays`` () =
+    let weights = [| |]
+    let prices = [| |]
+    let capacity = 8
+    let actual = solve prices weights capacity
+
+    match actual with
+    | Error _ -> Assert.Pass ()
+    | Ok value -> Assert.Fail ()
+
+
+
---
+++ OptimizationProblems.Dynamic.Bagpack/Library.fs
@@ -0,0 +1,48 @@
+﻿module OptimizationProblems.Dynamic.Bagpack
+
+
+let buildTable (prices: int array) (weights: int array) (capacity: int) =
+    let values = 
+        Array.zeroCreate<int> (capacity + 1)
+        |> List.replicate (1 + Array.length prices)
+    let prices' = [| 0; yield! prices |]
+    let weights' = [| 0; yield! weights |]
+
+    let folder (state: int * int array) (current: int array) = 
+        let rowIndex = fst state
+        let prev = snd state 
+        let result = 
+            current 
+            |> Array.mapi (fun index _ -> 
+                match index with 
+                | index when index < weights'.[rowIndex] ->
+                    prev.[index]
+                | index -> 
+                    (prev.[index], prices'[rowIndex] + prev.[index - weights'.[rowIndex]])
+                    ||> max
+            )
+        (result, (rowIndex + 1, result))
+
+    values 
+    |> List.skip 1
+    |> List.mapFold folder (1, values |> List.head)
+    |> fst
+
+
+
+let getPrice (table: int array list) = 
+    table
+    |> List.last
+    |> Array.last
+    
+    
+
+let solve (prices: int array) (weights: int array) (capacity: int) =
+    if (prices.Length <> weights.Length) then
+        Error "Invalid length"
+    elif (prices.Length = 0) then
+        Error "Arrays is empty"
+    else
+        buildTable prices weights capacity
+        |> getPrice
+        |> Ok
