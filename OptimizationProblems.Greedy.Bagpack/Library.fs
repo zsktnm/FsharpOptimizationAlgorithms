f0d98a58e79c710fe60ffce28f5d5e89a27b749d
Zsktnm <takraw@yandex.ru>
2023-11-16 16:30:31 +0500

    some refactoring

--- OptimizationProblems.Greedy.Bagpack/Library.fs
+++ OptimizationProblems.Greedy.Bagpack/Library.fs
@@ -1,16 +1,22 @@
 ﻿module OptimizationProblems.Greedy.Bagpack
 
+type BagState = {
+    Capacity: int;
+    Items: (int * int) list;
+}
+
 let solve prices weights capacity  = 
-    let folder state value =
-        let cap = fst state
-        let items = snd state
+
+    let folder (state: BagState) value =
         match value with
-        | (price, weight) when weight <= cap -> (cap - weight, (price, weight) :: items)
+        | (price, weight) when weight <= state.Capacity -> { 
+            Capacity = state.Capacity - weight;
+            Items = (price, weight) :: state.Items }
         | _ -> state
 
     weights 
     |> Seq.zip prices
     |> Seq.sortByDescending (fun pair -> fst pair / snd pair)
-    |> Seq.fold folder (capacity, [])
-    |> snd
+    |> Seq.fold folder { Capacity = capacity; Items = [] } 
+    |> fun bag -> bag.Items
     |> List.ofSeq
